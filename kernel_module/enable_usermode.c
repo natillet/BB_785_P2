4d5071128f3fa0d4fa5c1b4dedef45087217c100
Elene Trull <endahner@ncsu.edu>
2012-10-25 20:18:18 -0400

    handle counter interrupt setup better

--- kernel_module/enable_usermode.c
+++ kernel_module/enable_usermode.c
@@ -1,13 +1,19 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+int interrupt_setup;
+
 static int __init enable_usermode_init(void)
 {
 	/* enable user-mode access to the performance counter*/
 	asm ("MCR p15, 0, %0, C9, C14, 0\n\t" :: "r"(1));
+	/* read counter overflow interrupts (just in case)*/
+	asm ("MRC p15, 0, %0, C9, C14, 2\n\t" :: "r"(interrupt_setup));
 	/* disable counter overflow interrupts (just in case)*/
-	asm ("MCR p15, 0, %0, C9, C14, 2\n\t" :: "r"(0x8000000f));
-	printk(KERN_INFO "User mode enabled.\n");
+//	asm ("MCR p15, 0, %0, C9, C14, 2\n\t" :: "r"(0x8000000f));
+	asm ("MCR p15, 0, %0, C9, C14, 2\n\t" :: "r"(interrupt_setup));
+//	printk(KERN_INFO "User mode enabled.\n");
+	printk(KERN_INFO "User mode enabled: overflow interrupt setup is %x\n", interrupt_setup);
 	return 0;
 }
 
@@ -15,6 +21,9 @@ static void __exit exit_usermode(void)
 {
 	/* disable user-mode access to the performance counter*/
 	asm ("MCR p15, 0, %0, C9, C14, 0\n\t" :: "r"(0));
+	/* enable counter overflow interrupts (just in case)*/
+//	asm ("MCR p15, 0, %0, C9, C14, 1\n\t" :: "r"(0x8000000f));
+	asm ("MCR p15, 0, %0, C9, C14, 1\n\t" :: "r"(interrupt_setup));
 	printk(KERN_INFO "User mode disabled.\n");
 }
 
